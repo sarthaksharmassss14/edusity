4e6b81ad4592ad564bff36270e4812e3d232a621
Sarthak Sharma <sarthaksharma0234@gmail.com>
2025-04-18 20:37:19 +0530

    .

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,7 @@
 import "./About.css"
 import about_img from "../../assets/about.png"
 import play_icon from "../../assets/play-icon.png"
+import VideoPlayer from "../videoplayer/VideoPlayer"
 
 
 const About = ({setPlayerState}) => {
@@ -16,6 +17,7 @@ const About = ({setPlayerState}) => {
             <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.s</p>
             <p>Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
         </div>
+        
     </div>
   )
 }
